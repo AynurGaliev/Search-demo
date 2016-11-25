bb17af1b269c40f77e68d08eabe626a87c7f82e6
Aynur Galiev <aynurgaliev@Mac-mini-Aynur.local>
2016-11-25 13:57:17 +0300

    Initial Commit

---
+++ Search Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Search Demo
+//
+//  Created by Aynur Galiev on 25.ноября.2016.
+//  Copyright © 2016 Aynur Galiev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
